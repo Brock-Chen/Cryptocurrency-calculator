4c4aa066a4d92c05f82487f0b4e7b1f5d8b8db79
Brock <keney050700@gmail.com>
2025-02-02 11:35:03 +0800

    reBuild

--- client/src/services/data.service.js
+++ client/src/services/data.service.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "https://crypto-calculator.up.railway.app/api/data";
+const API_URL = "https://cryptocurrency-calculator-rgc6.onrender.com/api/data";
 
 class DataService {
   // 驗證使用者
